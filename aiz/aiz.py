43a1a758b66ed4d596ac23b277a62a557b90d6c0
MatPoliquin <mathieu.poliquin@gmail.com>
2020-05-11 17:17:55 +0800

    Changed graph color to green

--- aiz/aiz.py
+++ aiz/aiz.py
@@ -39,7 +39,7 @@ def MainLoop(win):
 
     curses.start_color()
     #COLOR_CYAN
-    curses.init_pair(1, curses.COLOR_CYAN, curses.COLOR_BLACK)
+    curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_BLACK)
     curses.noecho()
     win.nodelay(True)
 
